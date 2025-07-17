0308d76b9e82620e694bb64e4fb6e96404af9f05
AlexElaryan <alex.elaryan@icloud.com>
2025-07-17 14:00:52 +0400

    dep

--- script.js
+++ script.js
@@ -2,18 +2,23 @@ const burgerMenu = document.querySelector('.burger-menu');
 const burgerBtn = document.querySelector('.burger-btn');
 const burgerClose = document.querySelector('.burger-close');
 const a = document.querySelectorAll('.burger-menu ul li a');
-
-
+const intro = document.querySelector('.intro');
 
 burgerBtn.onclick = () => {
     burgerMenu.classList.add('active');
     document.body.style.overflow = 'hidden';
+    if (intro) {
+        intro.style.zIndex = '0';
+    }
 }
 
 
 function close() {
     burgerMenu.classList.remove('active');
     document.body.style.overflow = 'auto';
+    if (intro) {
+        intro.style.zIndex = '11';
+    }
 }
 
 burgerClose.onclick = () => close();
